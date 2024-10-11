e5cb866cea7b62a538a100a97295e21f545bec2e
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-10-11 11:46:43 +0200

    Update article-dao-mock.js

--- article/dao/article-dao-mock.js
+++ article/dao/article-dao-mock.js
@@ -4,7 +4,7 @@ class ArticleDAOMock extends DAOCrud {
     constructor() {
         super();
         this.data =  [
-            { id: '1', title: 'Premier article', desc: 'Contenu du premier article', author: 'Isaac', imgPath: 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg' },
+            { id: '1', title: 'Teletubies', desc: 'Contenu du premier article', author: 'Isaac', imgPath: 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg' },
             { id: '2', title: 'Deuxième article', desc: 'Contenu du deuxième article', author: 'Sanchez', imgPath: 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg' },
             { id: '3', title: 'Troisième article', desc: 'Contenu du troisième article', author: 'Toto', imgPath: 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg' }
         ]; // Stocke les données en mémoire
